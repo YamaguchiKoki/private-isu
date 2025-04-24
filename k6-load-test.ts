f3597ad718295a3a47b91c1c451dd8bfd0a0639c
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-04-24 23:01:44 +0900

    計測準備

--- k6-load-test.ts
+++ k6-load-test.ts
@@ -1,7 +1,13 @@
 import http from "k6/http";
 
-const BASE_URL = "http://localhost";
+import { sleep } from "k6"
+
+import { url } from "./config.js"
 
 export default function() {
-  http.get(`${BASE_URL}/`);
+  http.get(url("/initialized"), {
+    timeout: "10s",
+  });
+
+  sleep(1)
 }
